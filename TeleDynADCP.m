4f6fe11c9ea6303a864e8b9545e7a503c46de3e2
marcoturro <48596851+marcoturro@users.noreply.github.com>
2020-05-09 23:15:23 -0400

    Sunday Roast

---
+++ TeleDynADCP.m
@@ -0,0 +1,45 @@
+clear 
+%close all
+
+[file,path] = uigetfile;
+selectedfile = fullfile(path,file);
+load(selectedfile)
+
+%% this first section aims at obtaining the time step of the recordings
+
+altitude = 1-(Data.depth-Data.depth(1));
+mes = length(Data.dat(:,1));
+nb_of_sensors = length(altitude);
+
+dt = [diff(Data.dat(:,6)) , diff(Data.dat(:,5)) , diff(Data.dat(:,4)) ...
+            , diff(Data.dat(:,3)) , diff(Data.dat(:,2))];
+
+dt(dt<=0) = nan;
+
+for i = 1:5
+    try
+           ts(i) = dt(find(~isnan(dt(:,i)), 1),i);
+    catch
+           ts(i) = 0;
+    end
+end
+
+id = find(ts,1);
+t(1) = 0;
+stp = [1 60 60^2 60^2*24];
+clear dt
+dt = ts(id)*stp(id);
+
+for i=2:mes
+    t(i) = t(i-1) + dt;
+end
+
+for i=1:nb_of_sensors
+v(:,i) = Data.dat(:,6+i)/1000;
+end
+
+for i=1:nb_of_sensors
+thetha(:,i) = Data.dat(:,6+nb_of_sensors+i);
+end
+
+v(isnan(v))=0; thetha(isnan(thetha))=0;
--- Tracer.m
+++ Tracer.m
@@ -1,51 +1,10 @@
 clear 
 close all
 
-[file,path] = uigetfile;
-selectedfile = fullfile(path,file);
-load(selectedfile)
-
-%% this first section aims at obtaining the time step of the recordings
-
-altitude = 1-(Data.depth-Data.depth(1));
-mes = length(Data.dat(:,1));
-nb_of_sensors = length(altitude);
-
-dt = [diff(Data.dat(:,6)) , diff(Data.dat(:,5)) , diff(Data.dat(:,4)) ...
-            , diff(Data.dat(:,3)) , diff(Data.dat(:,2))];
-
-dt(dt<=0) = nan;
-
-for i = 1:5
-    try
-           ts(i) = dt(find(~isnan(dt(:,i)), 1),i);
-    catch
-           ts(i) = 0;
-    end
-end
-
-id = find(ts,1);
-t(1) = 0;
-stp = [1 60 60^2 60^2*24];
-clear dt
-dt = ts(id)*stp(id);
-
-for i=2:mes
-    t(i) = t(i-1) + dt;
-end
-
-for i=1:nb_of_sensors
-v(:,i) = Data.dat(:,6+i)/1000;
-end
-
-for i=1:nb_of_sensors
-thetha(:,i) = Data.dat(:,6+nb_of_sensors+i);
-end
-
-v(isnan(v))=0; thetha(isnan(thetha))=0;
+TeleDynADCP
 
 %% Here we set the parameters for advection and advect the tracers
-
+% and plot it
 day_r = 4; % on how many days are the particles advected
 
 days = round(dt*mes/60/60/24);
@@ -53,15 +12,27 @@ nb_of_realeases = floor(days/day_r);
 stp = 1+fix(1800/dt);          %set the time step to a minimum of 30mins
 partition = day_r/dt*60*60*24; % subdivide the time 
 n = floor(sqrt(nb_of_sensors));
+r = 1000;
 
 disp(['nb sensors: ' num2str(nb_of_sensors) ', release every ' ...
     num2str(day_r) ' days, measurement time [days]: ' num2str(days)])
 
-figure('Renderer', 'painters', 'Position', [500 500 500 400])
+figure('units', 'normalized', 'outerposition', [0 0 1 1])
 
-for j = 2:nb_of_sensors
+for j = 1:nb_of_sensors
 cnt = 1;
-subplot(n,n+1,j-1)
+s = subplot(n,n,j);
+hold on
+plot(r*cos(0:0.1:2*pi),r*sin(0:0.1:2*pi))
+txt = '0';
+text(r*cos(pi/2),r*sin(pi/2),txt)
+txt = '90';
+text(r*cos(0),r*sin(0),txt)
+txt = '180';
+text(r*cos(3/2*pi),r*sin(3/2*pi),txt)
+txt = '270';
+text(r*cos(pi),r*sin(pi),txt)
+
 for k = 1:nb_of_realeases
 k = k-1;
 x = zeros(nb_of_sensors,1) ; y = zeros(nb_of_sensors,1);
@@ -74,19 +45,26 @@ for i = 2:stp:partition
     catch
         disp('there was an error, moving on')
     end
+    
     th = atan(y(j)/x(j));
     dist = sqrt(x(j)^2+y(j)^2);
     if 900 < dist && 1100>dist
         
         X(j,cnt) = x(j); % cnt is to save the values which hit
-        Y(j,cnt) = y(j);% the condition of 1km
+        Y(j,cnt) = y(j); % the condition of 1km
         
-        scatter(1000*cos(th),1000*sin(th))
+        scatter(r*cos(th),r*sin(th))
+        xlabel('W - E [m]'); ylabel('S - N [m]');
+        title(['sensor n ' num2str(j) ' , ' num2str(altitude(j)) ' [m]'])
         xlim([-1200 1200])
         ylim([-1200 1200])
-        hold on
+        
         cnt = cnt+1;
     end
 end
 end
+if cnt == 1
+    delete(s)
+end
+    
 end
